288494695e5bbf10f53e42e980083665784e4775
matheus <matheusaltrao2@gmail.com>
2022-09-21 16:26:46 -0400

    First commit

---
+++ main.js
@@ -0,0 +1,52 @@
+function calculate() {
+  var height = document.getElementById('height').value
+  var weigh = document.getElementById('weigh').value
+  var imc = Number((weigh) / (height * height))
+
+  const flag = document.getElementById('flag')
+  const result = document.getElementById('result')
+  const ratingImc = document.getElementById('ratingImc')
+
+  if ((imc <= 0) || (imc == Number.POSITIVE_INFINITY)) {
+    result.innerHTML = ''
+    ratingImc.innerHTML = 'Preencha os campos corretamente.'
+
+  }
+  else if (imc <= 18.5) {
+    result.innerHTML = 'Seu IMC = ' + imc.toFixed(2)
+    ratingImc.innerHTML = 'A classificação do seu IMC é MAGREZA.'
+
+  } else if ((imc > 18.5) && (imc <= 24.9)) {
+    result.innerHTML = 'Seu IMC = ' + imc.toFixed(2)
+    ratingImc.innerHTML = 'A classificação do seu IMC é SAUDÁVEL.'
+
+  } else if ((imc > 25) && (imc <= 29.9)) {
+    result.innerHTML = 'Seu IMC = ' + imc.toFixed(2)
+    ratingImc.innerHTML = 'A classificação do seu IMC é  SOBREPESO.'
+
+  } else if ((imc > 30) && (imc <= 39.9)) {
+    result.innerHTML = 'Seu IMC = ' + imc.toFixed(2)
+    ratingImc.innerHTML = 'A classificação do seu IMC é OBESIDADE.'
+
+  } else if (imc > 40) {
+    result.innerHTML = 'Seu IMC = ' + imc.toFixed(2)
+    ratingImc.innerHTML = 'A classificação do seu IMC é OBESIDADE GRAVE.'
+
+  } else {
+    result.innerHTML = ''
+    ratingImc.innerHTML = 'Preencha os campos corretamente'
+  }
+
+  flag.classList.add('visible')
+
+
+
+  document.getElementById('height').value = ''
+  document.getElementById('weigh').value = ''
+
+}
+
+
+
+
+
